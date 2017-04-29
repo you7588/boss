c6b9870371f94c6f482ec0e58470076b6c44981f
you7588 <yo7588@gmail.com>
2017-04-30 00:34:37 +0800

    tell you--my carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require bootstrap/alert
-//= require bootstrap/dropdown
-//= require bootstrap/modal
+//= require bootstrap-sprockets
 //= require_tree .
